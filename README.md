9d1bf952bf741d8fd1692937413cee697ca83b33
Vitor Meriat <vitormeriat@gmail.com>
2023-10-24 18:09:28 +0100

    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# chess-is-awesome
+<div align="center">
+  <img width="240" src="https://upload.wikimedia.org/wikipedia/commons/a/a7/Chess_board_blank.svg">
+  <h1 style="margin-bottom:40px; margin-top:20px">Chess is Awesome ♞♟️</h1>
+</div>
+
+<a name="readme-top"></a>
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+<div align="center">
+  <img width="100" src="https://upload.wikimedia.org/wikipedia/commons/2/2b/AAA_SVG_Chessboard_and_chess_pieces_03.svg">
+</div>
+
+
+<!-- CONTRIBUTING -->
+## Contributing
+
+Contributions are what make the open source community such an amazing place to learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+If you have a suggestion that would make this better, please fork the repo and create a pull request. You can also simply open an issue with the tag "enhancement".
+Don't forget to give the project a star! Thanks again!
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+<!-- LICENSE -->
+## License
+
+Distributed under the MIT License. See `LICENSE.txt` for more information.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- CONTACT -->
+## Contact
+
+<div align="center">
+  <img width="50" src="https://www.vitormeriat.com/assets/images/profile.jpg">
+  <p style="margin-bottom:40px; margin-top:20px"><a href="http://www.vitormeriat.com" target="blank">Vitor Meriat</a> is a computer scientist who is passionate about creating software that will positively change the world we live in.</p>
+
+  <a class="fa fa-twitter" aria-hidden="true" href="https://twitter.com/vitormeriat" target="_blank"> twitter</a> | <a class="fa fa-instagram" aria-hidden="true" href="https://www.instagram.com/vitormeriat/" target="_blank"> instagram</a> | <a class="fa fa-linkedin" aria-hidden="true" href="https://www.linkedin.com/in/vitormeriat" target="_blank"> linkedin</a> | <a class="fa fa-youtube" aria-hidden="true" href="https://www.youtube.com/user/vitormeriat/" target="_blank"> youtube</a>
+</div>
+
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
---
+++ game.py
@@ -0,0 +1,185 @@
+class ChessGame:
+    """
+    Class representing a chess game with artificial intelligence.
+    https://codepal.ai/code-generator/query/QLz6r6Ct/create-chess-game-with-ai
+    Attributes:
+    - board (list): 2D list representing the chess board.
+    - current_player (str): The current player ('white' or 'black').
+    - game_over (bool): Flag indicating if the game is over.
+    - winner (str): The winner of the game ('white', 'black', or 'draw').
+ 
+    Methods:
+    - play(): Start the chess game.
+    - make_move(move: str): Make a move on the chess board.
+    - is_valid_move(move: str) -> bool: Check if a move is valid.
+    - is_checkmate() -> bool: Check if the current player is in checkmate.
+    - is_stalemate() -> bool: Check if the game is in stalemate.
+    - switch_player(): Switch to the next player.
+    - print_board(): Print the current state of the chess board.
+    """
+ 
+    def __init__(self):
+        """
+        Initialize the chess game.
+        """
+        self.board = [
+            ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
+            ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+            ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']
+        ]
+        self.current_player = 'white'
+        self.game_over = False
+        self.winner = None
+ 
+    def play(self):
+        """
+        Start the chess game.
+        """
+        print("Welcome to Chess!")
+        print("Type 'quit' to exit the game.")
+ 
+        while not self.game_over:
+            self.print_board()
+            move = input(f"{self.current_player.capitalize()} player, enter your move: ")
+ 
+            if move.lower() == 'quit':
+                self.game_over = True
+                print("Game over.")
+                break
+ 
+            if self.is_valid_move(move):
+                self.make_move(move)
+                if self.is_checkmate():
+                    self.game_over = True
+                    self.winner = self.current_player
+                    print(f"Checkmate! {self.winner.capitalize()} player wins.")
+                elif self.is_stalemate():
+                    self.game_over = True
+                    self.winner = 'draw'
+                    print("Stalemate! The game is a draw.")
+                else:
+                    self.switch_player()
+            else:
+                print("Invalid move. Try again.")
+ 
+    def make_move(self, move: str):
+        """
+        Make a move on the chess board.
+ 
+        Args:
+        - move (str): The move to be made in algebraic notation (e.g. 'e2e4').
+        """
+        start_col = ord(move[0]) - ord('a')
+        start_row = 8 - int(move[1])
+        end_col = ord(move[2]) - ord('a')
+        end_row = 8 - int(move[3])
+ 
+        self.board[end_row][end_col] = self.board[start_row][start_col]
+        self.board[start_row][start_col] = ' '
+ 
+    def is_valid_move(self, move: str) -> bool:
+        """
+        Check if a move is valid.
+ 
+        Args:
+        - move (str): The move to be checked in algebraic notation (e.g. 'e2e4').
+ 
+        Returns:
+        - bool: True if the move is valid, False otherwise.
+        """
+        start_col = ord(move[0]) - ord('a')
+        start_row = 8 - int(move[1])
+        end_col = ord(move[2]) - ord('a')
+        end_row = 8 - int(move[3])
+ 
+        piece = self.board[start_row][start_col]
+ 
+        # Check if the piece belongs to the current player
+        if (self.current_player == 'white' and piece.islower()) or (self.current_player == 'black' and piece.isupper()):
+            return False
+ 
+        # Check if the move is within the bounds of the board
+        if not (0 <= start_col <= 7 and 0 <= start_row <= 7 and 0 <= end_col <= 7 and 0 <= end_row <= 7):
+            return False
+ 
+        # Check if the move is valid for the piece
+        if piece.lower() == 'p':
+            # Pawn move
+            if start_col == end_col:
+                if piece.islower():
+                    if start_row - end_row == 1 and self.board[end_row][end_col] == ' ':
+                        return True
+                    elif start_row == 6 and start_row - end_row == 2 and self.board[end_row][end_col] == ' ' and self.board[end_row + 1][end_col] == ' ':
+                        return True
+                else:
+                    if end_row - start_row == 1 and self.board[end_row][end_col] == ' ':
+                        return True
+                    elif start_row == 1 and end_row - start_row == 2 and self.board[end_row][end_col] == ' ' and self.board[end_row - 1][end_col] == ' ':
+                        return True
+            elif abs(start_col - end_col) == 1 and abs(start_row - end_row) == 1:
+                if piece.islower() and self.board[end_row][end_col].isupper():
+                    return True
+                elif piece.isupper() and self.board[end_row][end_col].islower():
+                    return True
+        elif piece.lower() == 'r':
+            # Rook move
+            if start_col == end_col or start_row == end_row:
+                if start_col == end_col:
+                    step = 1 if start_row < end_row else -1
+                    for row in range(start_row + step, end_row, step):
+                        if self.board[row][start_col] != ' ':
+                            return False
+                else:
+                    step = 1 if start_col < end_col else -1
+                    for col in range(start_col + step, end_col, step):
+                        if self.board[start_row][col] != ' ':
+                            return False
+                return True
+        # Add code for other pieces (e.g. knight, bishop, queen, king) here
+ 
+        return False
+ 
+    def is_checkmate(self) -> bool:
+        """
+        Check if the current player is in checkmate.
+ 
+        Returns:
+        - bool: True if the current player is in checkmate, False otherwise.
+        """
+        # Add code to check for checkmate here
+        return False
+ 
+    def is_stalemate(self) -> bool:
+        """
+        Check if the game is in stalemate.
+ 
+        Returns:
+        - bool: True if the game is in stalemate, False otherwise.
+        """
+        # Add code to check for stalemate here
+        return False
+ 
+    def switch_player(self):
+        """
+        Switch to the next player.
+        """
+        self.current_player = 'black' if self.current_player == 'white' else 'white'
+ 
+    def print_board(self):
+        """
+        Print the current state of the chess board.
+        """
+        print("   a b c d e f g h")
+        print("  -----------------")
+        for row in range(8):
+            print(f"{8 - row} |{' '.join(self.board[row])}|")
+        print("  -----------------")
+        print("   a b c d e f g h")
+
+chess = ChessGame()
+chess.play()

584859797d3dd267e16d8fab0245bdb1d37a48ad
Vitor Meriat <vitormeriat@gmail.com>
2023-11-05 00:21:27 +0000

    Add py version

---
+++ moves_prediction/cnn_predict.py
@@ -0,0 +1,113 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+
+
+def import_data():
+    """
+    Here is an example of what a training dataset for a chess CNN might look like:
+
+    The `X_train` array contains the board states, where each board state is represented 
+    as a 3D array with dimensions `(6, 8, 8)`. The first dimension represents the six 
+    different types of pieces (pawn, knight, bishop, rook, queen, king), the second and 
+    third dimensions represent the position of each piece on the board 
+    (with values ranging from 0 to 7).
+
+    The `y_train` array contains the corresponding moves, where each move is represented 
+    as an integer between 0 and 72. These integers correspond to the 73 possible moves 
+    (including the \"null move\") that can be made from each board state.\n\nNote that the 
+    data in this example is just for demonstration purposes, and the actual data used for 
+    training a chess CNN would need to be much larger and more diverse in order to achieve 
+    good results.
+    """
+    # Load the dataset
+    X_train = np.load('X_train.npy')
+    y_train = np.load('y_train.npy')
+
+    # Print the shape of the data
+    print('X_train shape:', X_train.shape)
+    print('y_train shape:', y_train.shape)
+
+
+class ChessCNN(nn.Module):
+    """
+    In this example, we define a simple CNN model with three convolutional layers 
+    and three fully connected layers. We then load a dataset of chess board states 
+    and their corresponding moves, convert the data to PyTorch tensors, and train 
+    the model using the Adam optimizer and cross-entropy loss. Finally, we save the 
+    model's parameters to a file so that it can be used for prediction in the future.
+    Note that this is just a simple example, and there are many ways to modify and 
+    improve this model depending on your specific needs and dataset.
+    """
+
+    def __init__(self):
+        super(ChessCNN, self).__init__()
+
+        # Define the convolutional layers
+        self.conv1 = nn.Conv2d(6, 64, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
+        # Define the fully connected layers
+        self.fc1 = nn.Linear(128 * 8 * 8, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, 73)
+
+    def forward(self, x):
+        # Apply convolutional layers
+        x = nn.functional.relu(self.conv1(x))
+        x = nn.functional.relu(self.conv2(x))
+        x = nn.functional.relu(self.conv3(x))
+
+        # Flatten the output
+        x = x.view(x.size(0), -1)
+
+        # Apply fully connected layers
+        x = nn.functional.relu(self.fc1(x))
+        x = nn.functional.relu(self.fc2(x))
+        x = self.fc3(x)
+
+        return x
+
+
+# Load the dataset
+X_train = np.load('X_train.npy')
+y_train = np.load('y_train.npy')
+
+# Convert the dataset to PyTorch tensors
+X_train = torch.from_numpy(X_train).float()
+y_train = torch.from_numpy(y_train).long()
+
+# Define the model and optimizer
+model = ChessCNN()
+optimizer = optim.Adam(model.parameters())
+
+# Define the loss function
+criterion = nn.CrossEntropyLoss()
+
+# Train the model
+for epoch in range(10):
+    running_loss = 0.0
+    for i in range(len(X_train)):
+        # Zero the gradients
+        optimizer.zero_grad()
+
+        # Forward pass
+        outputs = model(X_train[i].unsqueeze(0))
+
+        # Calculate the loss
+        loss = criterion(outputs, y_train[i].unsqueeze(0))
+
+        # Backward pass and optimization
+        loss.backward()
+        optimizer.step()
+
+        # Print statistics
+        running_loss += loss.item()
+        if i % 1000 == 999:
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 1000))
+            running_loss = 0.0
+
+# Save the model
+torch.save(model.state_dict(), 'chess_cnn.pth')
